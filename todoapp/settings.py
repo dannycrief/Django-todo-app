61c71b1f413a9cec2d3b7607174d335b2eca87ee
Stepan Kozurak <step.kozbvb@gmail.com>
2020-05-30 16:57:57 +0200

    Improved settings

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -89,29 +89,29 @@ CACHES = {
 
         'OPTIONS': {
             # Use binary memcache protocol (needed for authentication)
-            'binary': True,
+            # 'binary': True,
             'username': username,
             'password': password,
-            'behaviors': {
-                # Enable faster IO
-                'no_block': True,
-                'tcp_nodelay': True,
-
-                # Keep connection alive
-                'tcp_keepalive': True,
-
-                # Timeout settings
-                'connect_timeout': 2000,  # ms
-                'send_timeout': 750 * 1000,  # us
-                'receive_timeout': 750 * 1000,  # us
-                '_poll_timeout': 2000,  # ms
-
-                # Better failover
-                'ketama': True,
-                'remove_failed': 1,
-                'retry_timeout': 2,
-                'dead_timeout': 30,
-            }
+            # 'behaviors': {
+            #     # Enable faster IO
+            #     'no_block': True,
+            #     'tcp_nodelay': True,
+            #
+            #     # Keep connection alive
+            #     'tcp_keepalive': True,
+            #
+            #     # Timeout settings
+            #     'connect_timeout': 2000,  # ms
+            #     'send_timeout': 750 * 1000,  # us
+            #     'receive_timeout': 750 * 1000,  # us
+            #     '_poll_timeout': 2000,  # ms
+            #
+            #     # Better failover
+            #     'ketama': True,
+            #     'remove_failed': 1,
+            #     'retry_timeout': 2,
+            #     'dead_timeout': 30,
+            # }
         }
     }
 }
